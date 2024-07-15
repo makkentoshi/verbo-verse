a0db9471cf891bfbe41809997bf9e15a4bbbd458
makkentoshi <pochta.zdraste@mail.ru>
2024-07-15 11:53:21 +0500

    chore: Update courses list and page components

--- db/queries.ts
+++ db/queries.ts
@@ -1,9 +1,28 @@
-import {cache} from "react"
-import db from "./drizzle"
+import { cache } from "react";
+import db from "./drizzle";
+import { auth } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
+import { userProgress } from "./schema";
+
+export const getUserProgress = cache(async () => {
+  const { userId } = await auth();
 
 
-export const getCourses =  cache(async () => {
-    const data = await db.query.courses.findMany()
+  if (!userId) {
+    return null;
+  }
 
-    return data;    
-})
+  const data = await db.query.userProgress.findFirst({
+    where: eq(userProgress.userId, userId),
+    with: {
+      activeCourse: true,
+    },
+  });
+  return data;
+});
+
+export const getCourses = cache(async () => {
+  const data = await db.query.courses.findMany();
+
+  return data;
+});
